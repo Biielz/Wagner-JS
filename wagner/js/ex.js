707a11c2a811166f45af372f923e30d5f777e19b
Bii <canaldesafios.1001@gmail.com>
2023-03-27 19:28:51 -0300

    teste 2

--- wagner/js/ex.js
+++ wagner/js/ex.js
@@ -1,8 +1,8 @@
 let num 
 
-for (i=0; i<=100; i++) {
+for (i=0; num<=100; i++) {
     if (num % 2 == 0) {
-        document.write("Par")
+        document.write("Par" + "<br>")
     }
     else {
         document.write("ímpar")
