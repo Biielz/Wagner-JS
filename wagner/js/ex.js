ea984d32727b7e9fa3557b898efbf38adfe47ec5
Bii <canaldesafios.1001@gmail.com>
2023-03-27 16:30:39 -0300

    tentando arrumar a sequencia

--- wagner/js/ex.js
+++ wagner/js/ex.js
@@ -1,11 +1,10 @@
-let num = -1
+let num = 1
 let num2 = 1
-let num3 = 0
+let num3
 
 for (i=0; i<=20; i++) {
     num3 = num + num2
     num = num2
     num2 = num3
+    document.write(num3 + "<br>")
 }
-
-document.write(num3 + "<br>")
