6520c79a14ce5713f763f4b12c1a34cc8fed1ddc
Bii <canaldesafios.1001@gmail.com>
2023-04-01 20:06:38 -0300

    yeehaw

--- wagner/js/ex.js
+++ wagner/js/ex.js
@@ -3,9 +3,9 @@ let num = 0
 for (i=0; i<=100; i++) {
     if (num % 2 == 0) {
         num = num * i
-        alert("Número" + num + "par<br>")
+        document.write("Número " + num + " par<br>")
     }
     else {
-        alert("ímpar")
+        document.write("ímpar")
     }
 }
