08373ffae549caec76bfa51285eba825488c7037
Bii <canaldesafios.1001@gmail.com>
2023-04-01 20:53:58 -0300

    teste do ex6

--- wagner/js/ex.js
+++ wagner/js/ex.js
@@ -1,5 +1,12 @@
-for (i=0; i<=100; i++) {
-    if (i % 2 == 0) {
-        document.write("Número " + i + " par<br>")
+
+for (i=0; i<=5; i++){
+    let info = parseFloat(prompt("qual seu sexo? "));
+    let alt = parseFloat(prompt("qual sua altura? "));
+    let mao = parseFloat(prompt("você é canhoto ou destro? "));
+    if (info == "homem") {
+        document.write("você é homem.<br>")
     }
-    }
+        if (info == "mulher") {
+            document.write("você é mulher.<br>")
+        }
+}
