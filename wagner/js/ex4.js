db75fe9942d8b0b75a650d623edc20edc630f136
Bii <canaldesafios.1001@gmail.com>
2023-04-02 20:28:13 -0300

    a

--- wagner/js/ex4.js
+++ wagner/js/ex4.js
@@ -3,11 +3,10 @@ let num2 = 0
 let num3 = 0
 
 for (i=0; i<=20; i++) {
-    document.write("<center>" , num3 = num + num2,
-    num = num2,
-    num2 = num3 + "</center>")
+    num3 = num + num2
+    num = num2
+    num2 = num3
     document.write(num3 + "<br>")
-    
 }
 
 document.write('<center><br><img src="images/fibo.png" alt="fibonacci"></center>')
